26b898318557c5d625ed710a1b73e5c20af589e2
skimnoise <skimnoise@gmail.com>
2024-09-09 00:39:04 -0400

    Added new fields for capsule and launch details to AccidentReport class

--- MarsIncidentReporter/MarsIncidentReporter/Models/AccidentReport.cs
+++ MarsIncidentReporter/MarsIncidentReporter/Models/AccidentReport.cs
@@ -9,5 +9,11 @@
         public string Description { get; set; }
         public string Severity { get; set; }
         public bool IsResolved { get; set; }
+        public string LaunchId { get; set; } 
+        public string LaunchName { get; set; }
+        public DateTime LaunchDate { get; set; }
+        public string LaunchpadName { get; set; }
+        public string CapsuleSerial { get; set; }
+        public string CapsuleName { get; set; }
     }
 }
